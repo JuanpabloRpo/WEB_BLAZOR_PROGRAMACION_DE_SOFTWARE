555fa7518f8064dc233e6e653d490d76753628d1
JuanRestrepo <juanparpola@gmail.com>
2025-05-28 22:43:56 -0500

    se agregan cambios graficos
    ahora cada reserva muestra el material reservado y el prestamo muestra quien lo presto y cual material

--- WEB_BLAZOR_PROGRAMACION_DE_SOFTWARE/Interfaces/IReservationService.cs
+++ WEB_BLAZOR_PROGRAMACION_DE_SOFTWARE/Interfaces/IReservationService.cs
@@ -4,8 +4,8 @@ namespace WEB_BLAZOR_PROGRAMACION_DE_SOFTWARE.Interfaces
 {
     public interface IReservationService
     {
-        Task<List<Reservation>> ListReservations();/*
-        Task<Reservation> GetReservation(int reservationId);*/
+        Task<List<Reservation>> ListReservations();
+        Task<Reservation> GetReservation(int reservationId);
         Task<List<Reservation>> GetReservationsUser(int UserId);
         Task<Boolean> CreateReservation(int materialId, int userId);
         /*Task<Boolean> ExtendReservation(int reservationId, int userId);*/
